856777a40659a5dc543694cc83905187ee2189c5
AJK4 <deepjk2005@gmail.com>
2021-02-19 15:44:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    // Logging To The Console
+    System.out.println("This is Java!");
+    // Int
+    int Int = 1;
+    // Double
+    double Double = 1.0;
+    // Boolean
+    boolean Boolean = true;
+    // Char
+    char Char = '1';
+    // String
+    String string = "One";
+    // Final
+    final int Ten = 10;
+  }
+}
